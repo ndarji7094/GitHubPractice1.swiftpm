bb52ae4c77c4bb725d5c12566cba00cadeaaabca
Neha Darji <ndarji7094@stu.d214.org>
2024-04-10 14:52:22 -0500

    fixed answer variable

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,11 @@ struct ContentView: View {
     
     @State var number2 = 22
     @State var number1 = 13
-    @State var answer = ""
+    @State var answer = 0
     
     var body: some View {
         TextField("Enter number 2", value: $number2, format: .number)
         TextField("Enter number 1", value: $number1, format: .number)
-        Text(answer)
+        Text("\(answer)")
     }
 }
