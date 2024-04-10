cc1256ec9afab07a639e3713fb1d56e498888f5a
Neha Darji <ndarji7094@stu.d214.org>
2024-04-10 14:44:30 -0500

    added answer variable; text view added

--- ContentView.swift
+++ ContentView.swift
@@ -4,9 +4,11 @@ struct ContentView: View {
     
     @State var number2 = 22
     @State var number1 = 13
+    @State var answer = ""
     
     var body: some View {
         TextField("Enter number 2", value: $number2, format: .number)
         TextField("Enter number 1", value: $number1, format: .number)
+        Text(answer)
     }
 }
